f3c99c7359c89c99ab6736e215f3d16ba71704e7
Tomi Lui <tomilui1221@gmail.com>
2021-05-22 16:57:34 -0700

    windows installer for tesseract added

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # camera-translation
 
+if you experience runtime error with googletrans, try installing the latest version of googletrans by running the following commands on windows.
+if `pip3 install googletrans` does not work,
+try `pip uninstall googletrans`
+then `pip install googletrans==3.1.0a0`
+to install the latest version.
 
-if ```pip3 install googletrans``` does not work,
-
-try ```pip uninstall googletrans```
-then ```pip install googletrans==3.1.0a0```
-
-to install latest versions that fix the googletrans package bug.
+to install a new version of tesseract-ocr on windows, visit the following link:
+https://github.com/UB-Mannheim/tesseract/wiki
---
+++ textDetection.py
@@ -0,0 +1,75 @@
+
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+img = cv2.imread('./assets/twttr.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (0, 0), fx=2, fy=2)
+# print(pytesseract.image_to_string(img))
+
+hImg, wImg, imgDimension = img.shape
+
+
+# Detect words
+boxes = pytesseract.image_to_data(img)
+for num, string in enumerate(boxes.splitlines()):
+
+    if num != 0:
+        boxInfo = string.split()
+        print(boxInfo)
+        if len(boxInfo) == 12:
+            word, x, y, w, h = boxInfo[11], int(boxInfo[6]), int(
+                boxInfo[7]), int(boxInfo[8]), int(boxInfo[9])
+
+            cv2.rectangle(
+                img,
+                (x, y),
+                (w+x, y+h),
+                (0, 0, 200), 1
+            )
+
+            cv2.putText(
+                img,
+                word,
+                (x, y-5),
+                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                1,
+                (0, 0, 200),
+                1
+            )
+
+
+# # detect individual characters
+# boxes = pytesseract.image_to_boxes(img)
+# for string in boxes.splitlines():
+
+#     boxInfo = string.split(' ')
+
+#     print(boxInfo)
+
+#     char, xbox, ybox, wbox, hbox = boxInfo[0], int(boxInfo[1]), int(
+#         boxInfo[2]), int(boxInfo[3]), int(boxInfo[4])
+
+#     # draw the ractangles on the image
+#     cv2.rectangle(
+#         img,
+#         (xbox, hImg-ybox),
+#         (wbox, hImg - hbox),
+#         (0, 0, 200), 1
+#     )
+
+#     cv2.putText(
+#         img,
+#         char,
+#         (xbox, hImg-ybox+15),
+#         cv2.FONT_HERSHEY_COMPLEX_SMALL,
+#         1,
+#         (0, 0, 200),
+#         1
+#     )
+
+cv2.imshow('Result', img)
+
+cv2.waitKey(0)
